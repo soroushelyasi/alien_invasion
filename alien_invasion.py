2a028ed0994bf46034319a273f6a43264e6ee0b9
soroushelyasi <soroush.elyasi343@gmail.com>
2024-12-19 00:00:41 +0330

    add tick and bg color

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,7 @@ class AlienInvasion:
     self.screen=pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
     self.clock = pygame.time.Clock()
+    self.bg_color = (230, 230, 230)
 #  ---------------------------------------------------------------
 # ****************************************************************   
 
@@ -21,9 +22,13 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+        self.clock.tick(60)
 #  ---------------------------------------------------------------
 # ****************************************************************   
 
