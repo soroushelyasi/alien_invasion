a9b8e94befa110d46e7923cfafa875fc9daa8394
soroushelyasi <soroush.elyasi343@gmail.com>
2024-12-20 15:55:15 +0330

    impliment movement

--- ship.py
+++ ship.py
@@ -7,12 +7,31 @@ class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.image = pygame.transform.scale(self.image, (self.image.get_width()*4, self.image.get_height()*5)) 
         self.rect = self.image.get_rect()
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        # Movement flags; start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left = False
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+#  ---------------------------------------------------------------
+# ****************************************************************
+
+    def update(self):
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+            
+        # Update rect object from self.x.
+        self.rect.x = self.x
 #  ---------------------------------------------------------------
 # ****************************************************************   
 
