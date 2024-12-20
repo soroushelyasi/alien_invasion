6385906b180387b6f5b6afeb7d1b9a4c7d62748c
soroushelyasi <soroush.elyasi343@gmail.com>
2024-12-20 13:49:03 +0330

    scall ship

--- ship.py
+++ ship.py
@@ -9,6 +9,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()*4, self.image.get_height()*5)) 
         self.rect = self.image.get_rect()
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
